2ffec888c77c83d94a059e1facba4ea76d40a848
Michael Dao <michaeltuandao@gmail.com>
2019-07-18 19:04:14 -0500

    update submit

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Postform />
       <Posts />
     </div>
   );
